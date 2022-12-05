f0bca4e122c5e97265adcd4de30183a48d3cf63b
Andrey Malkovskiy <andrey.malkovskiy@outlook.com>
2022-12-06 00:14:11 +0300

    initial

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <immintrin.h>
+
+
+int main() {
+    __m128d diff1 = _mm_set_pd(1.0, 3.0);
+    __m128d diff2 = _mm_set_pd(1.0, 1.0);
+
+    __m128d result = _mm_cmpneq_pd(diff1,diff2);
+    int test = _mm_movemask_pd(result);
+
+    if (test == 0) {
+        std::cout << "All elements are equal";
+    } else {
+        std::cout << "Not all elements are equal";
+    }
+    return 0;
+}
+
